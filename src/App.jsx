aba1709c9502f4ad001ec8d768346ee45061b7dc
charan-sai1 <sai782973@gmail.com>
2025-07-05 14:58:39 +0530

    Renaming Routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ import { SlTarget } from "react-icons/sl";
 function App() {
   // Your menu items
 const items = [
-  { name: "Current Affairs (2024-25)", label: "Current Affairs", route: "/current", icon: <FaGlobe /> },
-  { name: "Five Plans", label: "Five Year Plans", route: "/five", icon: <SlTarget /> },
-  { name: "Senus 2011", label: "Senus 2011", route: "/sensus", icon: <FaPeopleGroup /> }
+  { name: "Current Affairs (2024-25)", label: "Current Affairs", route: "/SSC/current", icon: <FaGlobe /> },
+  { name: "Five Plans", label: "Five Year Plans", route: "/SSC/five", icon: <SlTarget /> },
+  { name: "Senus 2011", label: "Senus 2011", route: "/SSC/sensus", icon: <FaPeopleGroup /> }
 ];
 
   return (
